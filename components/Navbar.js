5d88003456cd6f3d06002bfdcaa8997f5d4ded63
Hayden West <haydenw7@gmail.com>
2021-08-18 13:18:26 -0700

    began work on sidebar resizing

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,43 @@
+import { useState } from "react";
 import styles from "../styles/Navbar.module.css";
 
+import {
+  FaAlignCenter,
+  FaJediOrder,
+  FaBalanceScale,
+  FaChessKnight,
+} from "react-icons/fa";
+
 const Navbar = () => {
+  const [openNav, setOpenNav] = useState(false);
+
+  const openAndClose = (e) => {
+    console.log("works");
+    setOpenNav(!openNav);
+  };
+
   return (
     <div className={styles.sidebar}>
       <nav>
-        <div className="logo">
-          <h1>Header Bar</h1>
+        <FaAlignCenter
+          className={styles.sidebarIcon}
+          size={"15%"}
+          onClick={openAndClose}
+        />
+        <div>
+          <div className={styles.icons}>
+            <FaJediOrder size={40} />
+            {openNav ? <span> Go Here</span> : null}
+          </div>
+          <div className={styles.icons}>
+            <FaBalanceScale size={40} />
+            {openNav ? <span> Another Link</span> : null}
+          </div>
+          <div className={styles.icons}>
+            <FaChessKnight size={40} />
+            {openNav ? <span> What is This?</span> : null}
+          </div>
         </div>
-        <a>Home</a>
-        <a>About</a>
-        <a>Other</a>
       </nav>
     </div>
   );
