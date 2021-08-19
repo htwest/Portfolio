03e05bc156f4484e4e40994cf585425564f865fc
Hayden West <haydenw7@gmail.com>
2021-08-19 09:24:52 -0700

    began work on main page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,13 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main>CONTENT BOI</main>
+      <div className={styles.header}>
+        <p>Hello My Name is Hayden West</p>
+        <p>I'm a Full Stack Engineer</p>
+        <div className={styles.imgContainer}>
+          <img src="/Smiley.png"></img>
+        </div>
+      </div>
     </div>
   );
 }
