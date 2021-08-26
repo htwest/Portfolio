d83e1f8180d9d8277fd775c3df2130a08205887a
Hayden West <haydenw7@gmail.com>
2021-08-25 19:22:09 -0700

    added box animation for links

--- pages/Links.js
+++ pages/Links.js
@@ -3,7 +3,9 @@ import styles from "../styles/Links.module.css";
 const Links = ({ openNav }) => {
   return (
     <div className={openNav ? styles.containerOpen : styles.containerClose}>
-      <div className={styles.box}></div>
+      <div className={styles.boxOne}></div>
+      <div className={styles.boxTwo}></div>
+      <div className={styles.boxThree}></div>
     </div>
   );
 };
