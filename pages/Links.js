f3463dbe83ccb25f2cba66a7e47e2d9997574349
Hayden West <haydenw7@gmail.com>
2021-09-01 12:19:22 -0700

    added link cards with beginning animations

--- pages/Links.js
+++ pages/Links.js
@@ -1,11 +1,60 @@
+import { useState } from "react";
 import styles from "../styles/Links.module.css";
 
 const Links = ({ openNav }) => {
+  const [cardOne, setCardOne] = useState(false);
+  const [cardTwo, setCardTwo] = useState(false);
+  const [cardThree, setCardThree] = useState(false);
+
+  const hover = (e) => {
+    let card = e.target.id;
+    if (card === "cardOne") {
+      setCardOne(!cardOne);
+      console.log(cardOne);
+    }
+    if (card === "cardTwo") {
+      setCardTwo(!cardTwo);
+      console.log(cardTwo);
+    }
+    if (card === "cardThree") {
+      setCardThree(!cardThree);
+      console.log(cardThree);
+    }
+  };
+
   return (
     <div className={openNav ? styles.containerOpen : styles.containerClose}>
-      <div className={styles.boxOne}></div>
-      <div className={styles.boxTwo}></div>
-      <div className={styles.boxThree}></div>
+      <div className="animations">
+        <div className={styles.boxOne}></div>
+        <div className={styles.boxTwo}></div>
+        <div className={styles.boxThree}></div>
+      </div>
+      <div className={styles.cardContainer}>
+        <div
+          id="cardOne"
+          className={cardOne ? styles.cardHovered : styles.card}
+          onMouseEnter={hover}
+          onMouseLeave={hover}
+        >
+          <p>Test1</p>
+        </div>
+        <div
+          id="cardTwo"
+          className={cardTwo ? styles.cardHovered : styles.card}
+          onMouseEnter={hover}
+          onMouseLeave={hover}
+        >
+          <p>Test2</p>
+        </div>
+        <div
+          id="cardThree"
+          className={cardThree ? styles.cardHovered : styles.card}
+          onMouseEnter={hover}
+          onMouseLeave={hover}
+        >
+          <p>Test3</p>
+        </div>
+      </div>
     </div>
   );
 };
