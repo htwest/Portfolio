d68c0de4acea4438c6053916048c191f41a0bbba
Hayden West <haydenw7@gmail.com>
2021-09-16 12:15:50 -0700

    added images to public folder

---
+++ components/Skills/Layout.js
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return (
+    <div>
+      <h1>This Renders Correctly</h1>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/Skills.js
+++ pages/Skills.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
 import SkillsDeck from "../components/Skills/SkillsDeck";
+import Layout from "../components/Skills/Layout";
 
 import styles from "../styles/Skills/Skills.module.css";
 
 const Test = ({ openNav }) => {
+  const [layout, setLayout] = useState(false);
+
   return (
     <div className={openNav ? styles.containerOpen : styles.containerClose}>
       <div className={styles.bodyContainer}>
-        <SkillsDeck />
+        {layout ? null : <SkillsDeck />}
       </div>
     </div>
   );
