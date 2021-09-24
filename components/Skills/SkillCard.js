818f72b85e33e5e32f940f5ab05bd04c1094bd63
Hayden West <haydenw7@gmail.com>
2021-09-23 18:11:43 -0700

    cleaned up layout skill cards

--- components/Skills/SkillCard.js
+++ components/Skills/SkillCard.js
@@ -1,10 +1,21 @@
 import styles from "../../styles/Skills/SkillCard.module.css";
+import fetchSuite from "../../hooks/fetchSuite.js";
 
 const SkillCard = ({ skill }) => {
+  const suit = fetchSuite();
+
   return (
     <div className={styles.container}>
-      <img src="Skills/cardFront.png" className={styles.img} />
+      <p className={styles.txtTop}>
+        {suit}
+        {skill[0]}
+      </p>
+      <img className={styles.img} src="Skills/cardFront.png" alt="card-front" />
       <h1 className={styles.skill}>{skill}</h1>
+      <p className={styles.txtBottom}>
+        {suit}
+        {skill[0]}
+      </p>
     </div>
   );
 };
