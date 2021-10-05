9f9538444f91ad18646a1d81f64787c1c3544557
Hayden West <haydenw7@gmail.com>
2021-10-05 12:46:38 -0700

    added info to projects secions

--- components/Projects/ProjectView.js
+++ components/Projects/ProjectView.js
@@ -4,11 +4,14 @@ import styles from "../../styles/Projects/ProjectView.module.css";
 
 const ProjectView = ({ project }) => {
   const imgSrc = `./Projects/${project.title}.gif`;
+  const summary = project.summary;
 
   return (
     <div className={styles.container}>
       <h2>{project.title}</h2>
-      <p>{project.summary}</p>
+      {summary.map((block) => (
+        <p className={styles.block}>{block}</p>
+      ))}
       <Link href={project.link}>
         <a className={styles.link}>Check It Out!</a>
       </Link>
--- data/projects.js
+++ data/projects.js
@@ -2,24 +2,30 @@ export default {
   weather: {
     title: "Weather App",
     image: "Weather",
-    summary:
-      "A Basic Weather App that uses your IP address to lookup the weather for the user's current location.Uses IPData to obtain IP address and GeoLocation Uses WeatherAPI to obtain current weather data",
+    summary: [
+      "A Basic Weather App that uses your IP address to lookup the weather for the user's current location.",
+      "Uses IPData API to obtain IP address and GeoLocation while also using WeatherAPI to obtain current weather data. The base UI was kept simple in order to focus on the implementation of multitple asyncornous fetch requests within the Next.JS framework.",
+    ],
     link: "https://github.com/htwest/weather-app",
   },
 
   gallery: {
     title: "Photo Gallery",
     image: "WestGallery",
-    summary:
-      "A photo gallery that displays high quality photos in a column of rotating carousels. This app uses Firebase as a backend storage and refrence database.",
+    summary: [
+      "A photo gallery that displays high quality photos in a column of rotating carousels.",
+      "This app uses Firebase as a backend storage and refrence database. Swiper.JS was used to make the app mobile friendly, with CSS stylings built to handle devices off all sizes. Heavy emphasis was put into the front end layout, attemping for a sleek and simple design with professional flourish.",
+    ],
     link: "https://github.com/htwest/WestGallery",
   },
 
   catwalk: {
     title: "Catwalk",
     image: "Catwalk",
-    summary:
-      "Rainforest.com is the front-end of an imaginary e-commerce clothing website. It was built with React, Express, and Bootstrap. The web-app consists of 4 interactive components that provide product information and user feedback.",
+    summary: [
+      "Rainforest.com is the front-end and back end of an imaginary e-commerce clothing website.",
+      "As a full stack application, it was built with React, Express, and Bootstrap. The app consists of a Product Overview, a Related Products section, a Questions and Answers component, and a Ratings and Reviews element. Each utilizes requests to a third party server to preform the various interactive functions of each component.",
+    ],
     link: "https://github.com/htwest/hrbld15-FEC-BlueGrassBanjos",
   },
 };
